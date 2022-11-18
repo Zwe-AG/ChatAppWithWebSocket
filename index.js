9d6d0d3441956cdc528d01d29f536fcf109fbd65
Zwe <zwehtetag2019@gmail.com>
2022-11-18 19:10:17 +0630

    18 Nov 2022 Chat App With Web Socket

---
+++ index.js
@@ -0,0 +1,28 @@
+let express=require('express');
+let socket = require('socket.io');
+
+// app setup 
+let app=express();
+
+// server setup 
+let server=app.listen(4000,()=>{
+    console.log('project is running localhost:4000');
+});
+
+// route setup 
+app.get('/',function(res,req){
+    req.sendFile(__dirname+"/public/index.html");
+});
+
+// socket setup 
+let io=socket(server);
+io.on('connection',(socket)=>{
+    // console.log('socket connected'+socket.id);
+    socket.on('chat',(data)=>{
+        // console.log(data);
+        io.sockets.emit('chat',data);
+    });
+    socket.on('typing',(name)=>{
+        socket.broadcast.emit('typing',name);
+    });
+});
